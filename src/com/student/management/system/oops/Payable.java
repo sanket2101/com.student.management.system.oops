f8c626c05dc5b73461fac627cf9d85e136ac7f51
Sanket Patil <sanketpatil2101@gmail.com>
2025-12-30 01:26:45 +0530

    Introduced ENUM

--- src/com/student/management/system/oops/Payable.java
+++ src/com/student/management/system/oops/Payable.java
@@ -12,8 +12,20 @@ public interface Payable {
 
     public abstract double calculatePayment();
 
-    public default boolean processPayment(String paymentMode){
-        if(paymentMode.equalsIgnoreCase("CASH") || paymentMode.equalsIgnoreCase("UPI")|| paymentMode.equalsIgnoreCase("CARD")){
+//    public default boolean processPayment(String paymentMode){
+//        if(paymentMode.equalsIgnoreCase("CASH") || paymentMode.equalsIgnoreCase("UPI")|| paymentMode.equalsIgnoreCase("CARD")){
+//            System.out.println("Payment is Processing.......");
+//            System.out.println("Payment Successful");
+//            return true;
+//        }
+//        else{
+//            System.err.println("Something went wrong!!! ");
+//            return false;
+//        }
+//    }
+
+    public default boolean processPayment(PaymentMode paymentMode){
+        if(paymentMode==PaymentMode.CASH || paymentMode==PaymentMode.UPI|| paymentMode==PaymentMode.CARD){
             System.out.println("Payment is Processing.......");
             System.out.println("Payment Successful");
             return true;
---
+++ src/com/student/management/system/oops/PaymentMode.java
@@ -0,0 +1,5 @@
+package com.student.management.system.oops;
+
+public enum PaymentMode {
+    CASH,UPI,CARD;
+}
